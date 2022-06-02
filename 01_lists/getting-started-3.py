1e5113bb7cd910ef4ca1f619a5dc1a48d4bd5d3f
stefangrgv <stefan.grgv@gmail.com>
2022-06-02 10:29:51 +0300

    changed the data input and return type

--- 01_lists/getting-started-3.py
+++ 01_lists/getting-started-3.py
@@ -1,6 +1,8 @@
-def test_anagrams(a, b):
-    if sorted(a.lower()) == sorted(b.lower()):
-        return 'ANAGRAMS'
-    else:
-        return 'NOT ANAGRAMS'
+def is_anagrams():
+    a = input('Enter first word: ')
+    b = input('Enter second word: ')
 
+    if sorted(a.lower()) == sorted(b.lower()):
+        return True
+    else:
+        return False
