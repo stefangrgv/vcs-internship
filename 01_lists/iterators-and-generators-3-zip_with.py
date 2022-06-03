93add4c7874b04a618076c658ca41fe073f0978a
stefangrgv <stefan.grgv@gmail.com>
2022-06-03 11:35:30 +0300

    now using map

--- 01_lists/iterators-and-generators-3-zip_with.py
+++ 01_lists/iterators-and-generators-3-zip_with.py
@@ -1,11 +1,17 @@
+def concat3(x, y, z):
+    return x + y + z
+
 def zip_with(func, *args):
     if not callable(func):
         raise TypeError('First argument must be a function!')
 
     if len(args):
-        min_len = min([len(a) for a in args])
-        for i in range(min_len):
-            new_args = [a[i] for a in args]
-            yield func(*new_args)
+        yield list(map(func, *args))
     else:
         yield []
+
+
+first_names = ['John', 'Miles']
+last_names = ['Coltrane', 'Davis']
+spaces = [' '] * 2
+print(list(zip_with(concat3, first_names, spaces, last_names)))
