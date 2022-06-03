990f85857d0f72b961eb71f5c95bccbda56de306
stefangrgv <stefan.grgv@gmail.com>
2022-06-03 15:09:59 +0300

    added identity function

--- 01_lists/iterators-and-generators-2-iterate.py
+++ 01_lists/iterators-and-generators-2-iterate.py
@@ -2,18 +2,21 @@ def double(x):
     return 2 * x
 
 def iterate(func):
-    i = 0
-    def run_nested_function(*args):
-        if i == 0:
-            return args[0]
-        else:
-            result = func(*args)
-            for n in range(1, i):
-                result = func(result)
-            return result
+    def identity(x):
+        return x
 
+    def run_nested_function(x):
+        result = func(x)
+        for n in range(1, i):
+            result = func(result)
+        return result
+
+    i = 0
     while True:
-        yield run_nested_function
+        if i == 0:
+            yield identity
+        else:
+            yield run_nested_function
         i += 1
 
 i = iterate(double)
