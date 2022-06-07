0882ad0fab372161ab8a420204559b484c7f895a
stefangrgv <stefan.grgv@gmail.com>
2022-06-07 15:48:02 +0300

    оправих init-а, върнах self.operator
    не знам как да я реша без да пазя инфо за опрератора

--- 02_data_modeling/lazynumbers.py
+++ 02_data_modeling/lazynumbers.py
@@ -1,85 +1,99 @@
 class Lazy:
     def __init__(self, value):
-        self.value = value  # actual number if root node, else str containing operation between self.left and self.right
+        self.value = value
         self.left = None
         self.right = None
+        self.operator = None
 
     def __add__(self, other):
-        new_lazy = Lazy('+')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '+'
         return new_lazy
 
     def __radd__(self, other):
-        new_lazy = Lazy('+')
-        new_lazy.left = self
-        new_lazy.right = other
+        new_lazy = Lazy(None)
+        new_lazy.left = other
+        new_lazy.right = self
+        new_lazy.operator = '+'
         return new_lazy
 
     def __sub__(self, other):
-        new_lazy = Lazy('-')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '-'
         return new_lazy
 
     def __rsub__(self, other):
-        new_lazy = Lazy('-')
+        new_lazy = Lazy(None)
         new_lazy.left = other
         new_lazy.right = self
+        new_lazy.operator = '-'
         return new_lazy
 
     def __mul__(self, other):
-        new_lazy = Lazy('*')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '*'
         return new_lazy
 
     def __rmul__(self, other):
-        new_lazy = Lazy('*')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '*'
         return new_lazy
 
     def __truediv__(self, other):
-        new_lazy = Lazy('/')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '/'
         return new_lazy
 
     def __rtruediv__(self, other):
-        new_lazy = Lazy('/')
+        new_lazy = Lazy(None)
         new_lazy.left = other
         new_lazy.right = self
+        new_lazy.operator = '/'
         return new_lazy
 
     def __floordiv__(self, other):
-        new_lazy = Lazy('//')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '//'
         return new_lazy
 
     def __rfloordiv__(self, other):
-        new_lazy = Lazy('//')
+        new_lazy = Lazy(None)
         new_lazy.left = other
         new_lazy.right = self
+        new_lazy.operator = '//'
         return new_lazy
 
     def __mod__(self, other):
-        new_lazy = Lazy('%')
+        new_lazy = Lazy(None)
         new_lazy.left = self
         new_lazy.right = other
+        new_lazy.operator = '%'
         return new_lazy
 
     def __rmod__(self, other):
-        new_lazy = Lazy('%')
+        new_lazy = Lazy(None)
         new_lazy.left = other
         new_lazy.right = self
+        new_lazy.operator = '%'
         return new_lazy
 
     def __neg__(self):
-        new_lazy = Lazy('*')
+        new_lazy = Lazy(None)
         new_lazy.left = self
-        new_lazy.right = Lazy(-1)
+        new_lazy.right = -1
+        new_lazy.operator = '*'
         return new_lazy
 
     def __pos__(self):
@@ -98,7 +112,7 @@ class Lazy:
         return int(self.force())
 
     def force(self):
-        if not isinstance(self.value, str):
+        if not self.value is None:
             return self.value
 
         if isinstance(self.left, Lazy):
@@ -111,15 +125,15 @@ class Lazy:
         else:
             right_value = self.right
 
-        if self.value == '+':
+        if self.operator == '+':
             return left_value + right_value
-        if self.value == '-':
+        if self.operator == '-':
             return left_value - right_value
-        if self.value == '*':
+        if self.operator == '*':
             return left_value * right_value
-        if self.value == '/':
+        if self.operator == '/':
             return left_value / right_value
-        if self.value == '//':
+        if self.operator == '//':
             return left_value // right_value
-        if self.value == '%':
+        if self.operator == '%':
             return left_value % right_value
