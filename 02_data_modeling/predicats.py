c54def71c960da270e50adaa889050558a2dd839
stefangrgv <stefan.grgv@gmail.com>
2022-06-09 09:09:19 +0300

    funcs of predicators no longer return a predicator

--- 02_data_modeling/predicats.py
+++ 02_data_modeling/predicats.py
@@ -1,3 +1,5 @@
+import operator
+
 class Predicate:
     def __init__(self, func, argument=()):
         self.func = func
@@ -7,16 +9,16 @@ class Predicate:
         return self.func(arg)
 
     def __and__(self, other):
-        return Predicate(func = lambda x: (self.func(x) * other.func(x)), argument=self.argument)
+        return lambda x: self.func(x) * other.func(x)
 
     def __or__(self, other):
-        return Predicate(func = lambda x: (self.func(x) + other.func(x)), argument=self.argument)
+        return lambda x: self.func(x) + other.func(x)
 
     def __invert__(self):
-        return Predicate(func = lambda x: not self.func(x), argument = self.argument)
+        return lambda x: not self.func(x)
 
     def __rshift__(self, other):
-        return Predicate(func = lambda x: (not self.func(x)) + other.func(x), argument=self.argument)
+        return lambda x: not self.func(x) + other.func(x)
 
 def gt(x):
     return Predicate(lambda n: n > x, x)
@@ -36,16 +38,19 @@ def present():
 def pred(function):
     return Predicate(lambda n: bool(function(n)))
 
+def constructor(f, g, op):
+    return lambda x: op(f(x), g(x))
+
 def for_any(*predicates):
     result = predicates[0]
     for pred in predicates[1:]:
-        result = result | pred
+        result = constructor(pred.func, result, operator.add)
 
     return result
 
 def for_all(*predicates):
-    result = predicates[0]
+    result = predicates[0].func
     for pred in predicates[1:]:
-        result = result & pred
+        result = constructor(pred.func, result, operator.mul)
 
     return result
