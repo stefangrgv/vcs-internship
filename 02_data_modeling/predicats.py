dbae02282e5a979cb0273a750f83072e963fa0c5
stefangrgv <stefan.grgv@gmail.com>
2022-06-09 14:01:55 +0300

    changed for_any, for_all

--- 02_data_modeling/predicats.py
+++ 02_data_modeling/predicats.py
@@ -1,6 +1,7 @@
 import operator
 import functools
 
+
 class Predicate:
     def __init__(self, func):
         self.func = func
@@ -9,43 +10,51 @@ class Predicate:
         return self.func(x)
 
     def __and__(self, other):
-        return lambda x: self(x) and other(x)
+        return lambda x: self(x) & other(x)
 
     def __rand__(self, other):
-        return lambda x: self(x) and other(x)
+        return lambda x: self(x) & other(x)
 
     def __or__(self, other):
-        return lambda x: self(x) or other(x)
+        return lambda x: self(x) | other(x)
 
     def __ror__(self, other):
-        return lambda x: self(x) or other(x)
+        return lambda x: self(x) | other(x)
 
     def __invert__(self):
         return lambda x: not self(x)
 
     def __rshift__(self, other):
-        return lambda x: not self(x) or other(x)
+        return lambda x: (not self(x)) | other(x)
+
 
 def gt(x):
     return Predicate(lambda n: n > x)
 
+
 def lt(x):
     return Predicate(lambda n: n < x)
+
 
 def eq(x):
     return Predicate(lambda n: n == x)
 
+
 def oftype(x):
     return Predicate(lambda n: isinstance(n, x))
+
 
 def present():
     return Predicate(lambda n: n is not None)
 
+
 def pred(function):
     return Predicate(lambda n: function(n))
 
+
 def for_any(*predicates):
-    return functools.reduce(operator.or_, predicates)
+    return lambda x: any(p(x) for p in predicates)
+
 
 def for_all(*predicates):
-    return functools.reduce(operator.and_, predicates)
+    return lambda x: all(p(x) for p in predicates)
