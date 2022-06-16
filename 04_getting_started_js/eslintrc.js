27853abe02b8a9cb4906ab318920c87a47f7825f
stefangrgv <stefan.grgv@gmail.com>
2022-06-16 10:24:47 +0300

    Add files via upload

---
+++ 04_getting_started_js/eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "root": true,
+    "extends": [
+        "eslint:recommended"
+    ],
+    "rules": {
+          "space-before-function-paren": ["error", "always"],
+          "space-before-blocks": ["error", "always"],
+          "quotes": [2, "single", { "avoidEscape": true }]
+    }
+}
