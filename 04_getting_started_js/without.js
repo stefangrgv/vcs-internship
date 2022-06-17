5ff43c27dc9d52cb4c9fd910078427ed219a4fe9
stefangrgv <stefan.grgv@gmail.com>
2022-06-17 11:07:46 +0300

    js functional problems part 1 (#18)
    * js functional problems part 1
    
    * if-else -> one line, corrected style to camelCase
    
    * fixed linter errors
    
    * fixed linter errors
    
    * fixed linter errors
    
    * fixed linter errors
    
    * forEach -> reduce
    
    * fixed linter errors
    
    * testing linter
    
    * Add files via upload
    
    * Update and rename eslintrc.json to .eslintrc.json
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * testing linter
    
    * Delete .eslintrc.json
    
    * Delete .eslintrc.js
    
    * generate configfile
    
    * Add files via upload
    
    * Delete .eslintrc.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Add files via upload
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete package.json
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Delete package.json
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * Add files via upload
    
    * Delete .eslintrc.js
    
    * Add files via upload
    
    * Rename eslintrc.js to .eslintrc.js
    
    * configured linter
    
    * corrected errors
    
    * if-else -> 1 line
    
    * added babel install
    
    * install babel/core
    
    * moved depends to package.json
    
    * removed babel-eslint parser
    
    * Setup project's package.json correctly

---
+++ 04_getting_started_js/without.js
@@ -0,0 +1,17 @@
+var without = function (exclude, arr) {
+    return arr.filter(function (el) {
+        return !exclude.includes(el);
+    });
+}
+
+var arrayEquals = function (a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every(function (val, index) {
+            return val === b[index];
+        });
+}
+
+var expectedResult = [1, 2, 3, 4];
+console.assert(arrayEquals(without([5, 6], [1, 2, 3, 4, 5, 6]), expectedResult));
