6c7443621274588f44c91d77f01e60080df95d4f
Stefan Georgiev <stefan.grgv@gmail.com>
2022-06-20 17:31:20 +0300

    added point_sky, robot

---
+++ 04_getting_started_js/point_sky.js
@@ -0,0 +1,52 @@
+class Point{
+    constructor(x, y) {
+        if (!(typeof x === 'number') || !(typeof y === 'number')) {
+            throw Error('Point coordinates (x, y) must be numbers!');
+        }
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    xInc() {
+        this._x += 1;
+    }
+
+    xDec() {
+        this._x -= 1;
+    }
+
+    yInc() {
+        this._y += 1;
+    }
+
+    yDec() {
+        this._y -= 1;
+    }
+}
+
+Point.prototype.equals = function (other) {
+    if (!(other instanceof Point)) {
+        throw Error('Error: method equals() requires an argument of type Point');
+    }
+    if ((this.x === other.x) && (this.y === other.y)) {
+        return true;
+    }
+    return false;
+}
+
+point1 = new Point(0,2);
+point1.xInc();
+point1.xInc();
+point1.yInc();
+
+point2 = new Point(2,3);
+
+console.assert(point1.equals(point2));
