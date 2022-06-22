09b32a6ec3848bec2f1f1aeea1e9fae9e18f1a1e
Stefan Georgiev <stefan.grgv@gmail.com>
2022-06-22 16:05:29 +0300

    finished logic of pizzeria.js

--- 04_getting_started_js/pizzeria.js
+++ 04_getting_started_js/pizzeria.js
@@ -1,5 +1,3 @@
-var orders = [];
-
 class Pizza {
     constructor (name, cost, timeToMake) {
         if (!((typeof cost  === 'number') && Number.isInteger(cost))) {
@@ -14,6 +12,14 @@ class Pizza {
         this._cost = cost;
         this._timeToMake = timeToMake;
     }
+
+    getName () {
+        return this._name;
+    }
+
+    getTimeToMake () {
+        return this._timeToMake;
+    }
 }
 
 class PizzaOrder {
@@ -22,40 +28,75 @@ class PizzaOrder {
             throw Error('PizzaOrder requires an argument of type Pizza');
         }
 
+        ordersMade += 1;
         this._pizza = pizza;
-
-        var lastOrder = orders.at(-1);
-        if (typeof lastOrder === 'undefined') {
-            this._id = 1;
-        } else {
-            this._id = lastOrder.id + 1;
-        }
-        orders.push({'id': this._id, 'pizza': this._pizza, 'status': 'waiting'});
+        this._id = ordersMade;
     }
 
     getId () {
         return this._id;
     }
 
-    start () {
-        var currentOrder = orders.find( (o) => o['id'] === this._id );
-        currentOrder['status'] = 'being prepared';
+    //
+    getPizza () {
+        return this._pizza;
     }
 
-    ready = function ( callback ){
-        // does something
-        var currentOrder = orders.find( (o) => o['id'] === this._id );
-        currentOrder['status'] = 'ready';
+    start () {
+        kitchenBusy = true;
+        console.log(`Started making a ${this.getPizza().getName()}!`);
+        setTimeout( this.ready, this.getPizza().getTimeToMake(), notifyPizzaReady );
+    }
+
+    ready ( callback ) {
+        callback(this._pizza, queue[0]);
+        queue.shift();
+        kitchenBusy = false;
     }
 }
 
-var pizza = new Pizza("Peperoni", 100 /*cost*/, 2000 /*timeToMake in ms = 2 seconds */);
+function notifyPizzaReady (pizza, order) {
+    console.log(`Order #${order.getId()} (${order.getPizza().getName()}) is ready!`);
+}
 
-var order1 = new PizzaOrder(pizza);
-var order2 = new PizzaOrder(pizza);
-var order3 = new PizzaOrder(pizza);
-order1.start();
-order1.ready();
-order2.start();
+function placeOrder () {
+    let randomPizzaIndex = generateRandomInt(pizzas.length - 1);
+    let order = new PizzaOrder(pizzas[randomPizzaIndex]);
+    queue.push(order);
+    console.log(`Placed a new order (ID=${order.getId()}, #${queue.length} in queue)!`);
+}
 
-console.log(orders)
+function generateRandomBool () {
+    return ( generateRandomInt(1) == 1 );
+}
+
+function generateRandomInt (max) {
+    // generates a random integer in the range [0, max]
+    let rand = Math.random();
+    rand = Math.floor( rand * (max+1) );
+
+    return rand;
+}
+
+function tick () {
+    let randBool = generateRandomBool();
+    if (randBool) {
+        placeOrder();
+    }
+
+    if (!(kitchenBusy) && (queue.length > 0)) {
+        queue[0].start();
+    }
+}
+
+var ordersMade = 0;
+var queue = [];
+var kitchenBusy = false
+
+var peperoni = new Pizza('Peperoni', 100 /*cost*/, 2000 /*timeToMake in ms = 2 seconds */);
+var vegetariana = new Pizza('Vegetariana', 70, 1500);
+var quattroStagioni = new Pizza('Quattro Stagioni', 120, 2500);
+
+var pizzas = [peperoni, vegetariana, quattroStagioni]
+
+setInterval(tick, 1000);
