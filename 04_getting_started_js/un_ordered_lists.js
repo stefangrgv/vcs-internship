8cc6ab6704a9557aa8de166538e67dcf6ac10646
Stefan Georgiev <stefan.grgv@gmail.com>
2022-06-28 10:41:05 +0300

    added missing files

---
+++ 04_getting_started_js/un_ordered_lists.js
@@ -0,0 +1,32 @@
+var ul = function (items) {
+  return '<ul>\n' + items
+                      .map(item => `\t<li>${item['label']}</li>\n`)
+                      .join('')
+                    + '</ul>'
+};
+
+
+var ol = function (items) {
+  return '<ol>\n' + items
+                      .map(item => `\t<li>${item['label']}</li>\n`)
+                      .join('')
+                    + '</ol>'
+};
+
+
+var items = [{ 'label' : 'Item 1'},
+             { 'label' : 'Item 2', 'children' : [
+                {
+                    'label' : 'Level 2 of Item 2'
+                },
+                {
+                    'label' : 'Another level 2'
+                }
+             ]}];
+
+
+var htmlul = ul(items);
+console.log(htmlul);
+
+var htmlol = ol(items);
+console.log(htmlol);
