a2d3acada29a9bcf435da113073283f54c395782
stefangrgv <stefan.grgv@gmail.com>
2022-06-28 16:21:33 +0300

    first attempt on shapes (#26)

--- 04_getting_started_js/shapres.js
+++ 04_getting_started_js/shapres.js
@@ -1,53 +1,71 @@
-function Shape(type) {
+function Shape (type) {
   this.type = type
 
-  this.getType = function() {
+  this.getType = function () {
     return type
   }
 }
 
-Shape.prototype.area = function() {
-  throw new Error("Cannot call area of Shape. Use subclasses!")
+Shape.prototype.area = function () {
+  throw new Error('Cannot call area of Shape. Use subclasses!')
 }
 
-function Rectangle(size) {
-  Shape.call(this, "rectangle")
+class Rectangle extends Shape {
+  constructor (side) {
+    if (side <= 0) {
+      throw Error('Side must be > 0');
+    }
+    super('rectangle');
+    this.side = side;
+  }
 
-  this.size = size
+  area () {
+    return Math.pow(this.side, 2);
+  }
 }
 
-Rectangle.prototype = Object.create(Shape.prototype)
-Rectangle.prototype.constructor = Rectangle
+class Triangle extends Shape {
+  constructor (a, b, c) {
+    if ((a <= 0) || (b <= 0) || (c <= 0)) {
+      throw Error('All sides must be > 0')
+    }
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+      throw Error('Triangle cannot exist: sides A + B must always be greater than side C');
+    }
+    super('triangle');
+    this.a = a;
+    this.b = b;
+    this.c = c; 
+  }
 
-Rectangle.prototype.area = function() {
-  return this.size * this.size
+  area () {
+    let s = 0.5 * (this.a + this.b + this.c);
+    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  }
 }
 
+class Circle extends Shape {
+  constructor (r) {
+    if (r <= 0) {
+      throw Error('Radius must be > 0');
+    }
+    super('circle');
+    this.r = r; 
+  }
 
-function Triangle(h, b) {
-  Shape.call(this, "triangle")
-  this.h = h
-  this.b = b
+  area () {
+    return Math.PI * Math.pow(this.r, 2);
+  }
 }
 
-Triangle.prototype = Object.create(Shape.prototype)
-Triangle.prototype.constructor = Triangle
+let r = new Rectangle(5);
+console.log(r.getType());
+console.log(r.area());
 
-Triangle.prototype.area = function() {
-  return this.h * this.b / 2
-}
+let t = new Triangle(2,4,5);
+console.log(t.getType());
+console.log(t.area());
 
-function Circle(r) {
-  Shape.call(this, "circle")
-  this.r = r
-}
-
-Circle.prototype = Object.create(Shape.prototype)
-Circle.prototype.constructor = Circle
-
-Circle.prototype.area = function() {
-  return this.r * this.r / 3.14
-}
-
-var c = new Circle(5)
-console.log(c.area())
+let c = new Circle(10);
+console.log(c.getType());
+console.log(c.area());
