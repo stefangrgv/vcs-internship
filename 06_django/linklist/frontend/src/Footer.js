e49f032325bc703b5c9da8555c837cf7200c3919
Stefan Georgiev <stefan.grgv@gmail.com>
2022-07-15 16:47:38 +0300

    configured django server with react

---
+++ 06_django/linklist/frontend/src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="Footer">
+            <h5>Generic Footer</h5>
+        </div>
+    )
+}
+
+export default Footer
---
+++ 06_django/linklist/frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Header from './Header';
+import Footer from './Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />
+    <App />
+    <Footer />
+  </React.StrictMode>
+);
