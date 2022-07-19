6d8a7bed13742da2d7ab8da96f657857d8537241
Stefan Georgiev <stefan.grgv@gmail.com>
2022-07-19 11:34:10 +0300

    axios.get now makes api call

--- 06_django/linkbundle/frontend/src/App.js
+++ 06_django/linkbundle/frontend/src/App.js
@@ -1,12 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h3>react stuff</h3>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      data: {},
+    }
+  }
+
+  getData () {
+    axios.get('/api/')
+    .then((response) => {
+      this.setState(
+        {isLoaded: true, data: response.data[0]}
+      )
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  render () {
+    this.getData();
+
+    return (
+      <div className="App">
+        <h3>{this.state.isLoaded ? this.state.data['url'] : 'loading...'}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
